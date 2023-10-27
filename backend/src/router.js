5264b86de290dc4d67e52d0ecd9402c6ae786ee9
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-10-27 11:30:01 +0200

    Feature diaplsy movies + database movies

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,23 +8,26 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemControllers module for handling item-related operations
-
+const itemControllers = require("./controllers/itemControllers");
 // Route to get a list of items
-router.get("/items", (req, res) => {
+
+// Route to get a specific item by ID
+// router.get("/items/:id", itemControllers.read);
+router.get("/movies/:id", itemControllers.read, (req, res) => {
   client
-    .query("select * from item")
-    .then((result) => {
-      res.status(200).json(result[0]);
+    .query("select * from movie where id = ?")
+    .then(([movie]) => {
+      if (movie[0] != null) {
+        res.json(movie[0]);
+      } else {
+        res.sendStatus(404);
+      }
     })
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
     });
 });
-
-// Route to get a specific item by ID
-// router.get("/items/:id", itemControllers.read);
-
 // Route to add a new item
 // router.post("/items", itemControllers.add);
 
