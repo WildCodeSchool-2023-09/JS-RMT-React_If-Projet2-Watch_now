4ff6324545847c97803bcf78386795586c3c92fc
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-10-27 11:42:50 +0200

    bug display

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,26 +8,21 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
 // Route to get a list of items
-
-// Route to get a specific item by ID
-// router.get("/items/:id", itemControllers.read);
-router.get("/movies/:id", itemControllers.read, (req, res) => {
+router.get("/movies", (req, res) => {
   client
-    .query("select * from movie where id = ?")
-    .then(([movie]) => {
-      if (movie[0] != null) {
-        res.json(movie[0]);
-      } else {
-        res.sendStatus(404);
-      }
+    .query("select * from movie")
+    .then((result) => {
+      res.status(200).json(result[0]);
     })
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
     });
 });
+// Route to get a specific item by ID
+// router.get("/items/:id", itemControllers.read);
+
 // Route to add a new item
 // router.post("/items", itemControllers.add);
 
