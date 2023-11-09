ff3c923e45bbd406a2426c0a64fa18f0c6937799
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-11-09 16:18:37 +0100

    Css + page active done modal menu nav

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../style/Home.css";
 import NavBar from "../components/NavBar";
+import NavbarModal from "../components/NavbarModal";
 
 function Home() {
   return (
     <>
       <NavBar />
+      <NavbarModal />
       <div />
     </>
   );
