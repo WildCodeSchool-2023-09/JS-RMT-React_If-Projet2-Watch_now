bdf382404c7f0b2dfd7db62b5e5366eb719a0b32
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-11-09 16:15:38 +0100

    Css + page active done modal menu nav

--- frontend/src/pages/MyList.jsx
+++ frontend/src/pages/MyList.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+import NavbarModal from "../components/NavbarModal";
 
 function MyList() {
   return (
     <div>
       <h1>MyList</h1>
+      <NavBar />
+      <NavbarModal />
     </div>
   );
 }
