6cf578d926300fa9e6f5836ed42c79e7f33e7feb
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-11-10 12:30:36 +0100

    debug css + conflits

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import axios from "axios";
 import AllMovies from "./pages/AllMovies";
 import MoviePage from "./pages/MoviePage";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
