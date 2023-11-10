1abb53db75050f95340d57a60bdbad0e8c5233ca
iNaeRoZ <naeroz@outlook.fr>
2023-11-10 11:33:34 +0100

    finition css

--- frontend/src/pages/MoviePage.jsx
+++ frontend/src/pages/MoviePage.jsx
@@ -1,60 +1,40 @@
 import React from "react";
-import { useLoaderData, Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "../style/Circular.css";
 import "../style/MoviePage.css";
 
 function MoviePage() {
   const movie = useLoaderData();
-
+  const normalizedVote = (movie.vote_average / 10) * 100;
   return (
     <main className="main-moviePage">
-      <div>
-        <Link to="/">Home</Link>
-        <h1>Details du film</h1>
-
-        <h2>{movie.title}</h2>
-        <div className="image-overlay-container">
-          <img
-            className="overlay-image"
-            src={movie.backdrop_path}
-            alt={movie.title}
+      <div className="background-image">
+        <img src={movie.backdrop_path} alt={movie.title} />
+        <div />
+      </div>
+      <div className="overlay">
+        <h1 className="Title">{movie.title}</h1>
+        <div className="overlay-buttons">
+          <span>{movie.genre_ids}</span>
+          <span>{movie.release_date}</span>
+          <span>{movie.vote_average}</span>
+        </div>
+        <img
+          className="poster-movie"
+          src={movie.poster_path}
+          alt={movie.title}
+        />
+        <p className="description-movie">{movie.overview}</p>
+        <div className="vote-movie">
+          <span id="number">{movie.vote_average}</span>
+          <CircularProgressbar
+            value={normalizedVote}
+            styles={buildStyles({
+              pathColor: "#FE0A3D",
+              trailColor: "#0F1014",
+            })}
           />
-          <img src={movie.poster_path} alt={movie.title} />
-          <ol>
-            <li>{movie.genre_ids}</li>
-            <li>{movie.release_date}</li>
-            <li>{movie.vote_average}</li>
-          </ol>
-          <p className="overlay-content">{movie.overview}</p>
-          <div className="skill">
-            <div className="outer">
-              <div className="inner">
-                <div id="number">{movie.vote_average}</div>
-              </div>
-            </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              version="1.1"
-              width="160px"
-              height="160px"
-            >
-              <circle cx="80" cy="80" r="70" />
-            </svg>
-          </div>
-
-          <ol>
-            <li>
-              {/* <button>add to cart</button>
-            </li>
-            <li>
-              <button>logo tv</button>
-            </li>
-            <li>
-              <button>logo j'aime</button>
-            </li>
-            <li>
-  <button>logo favori</button> */}
-            </li>
-          </ol>
         </div>
       </div>
     </main>
