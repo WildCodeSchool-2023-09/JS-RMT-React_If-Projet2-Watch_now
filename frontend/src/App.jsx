7e98545ee0db97de5e98a58fe78fa68ca6a165a9
Zonghero Jean Marc <jm.zonghero@gmail.com>
2023-11-17 11:49:31 +0100

    Modification des trois observations faites Thibault et Maxim

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,9 @@
-import axios from "axios";
 import "./App.css";
 import Contact from "./components/Contact";
-import AllProduct from "./pages/AllProduct";
-import OneProduct from "./pages/OneProduct";
 
 function App() {
-  const load = () => {
-    axios
-      .get("http://localhost:3310/api/items")
-      .then((result) => {
-        console.info(result.data);
-      })
-      .catch((err) => console.error(err));
-  };
   return (
     <div className="App">
-      <button type="button" onClick={() => load()}>
-        load
-      </button>
-      <AllProduct />
-      <OneProduct />
       <Contact />
     </div>
   );
--- frontend/src/pages/OneProduct.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function OneProduct() {
-  return <div>OneProduct</div>;
-}
-
-export default OneProduct;
