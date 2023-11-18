2f4fc47e0bfc0dec100a339dfd99f55b9709cd01
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-11-18 15:32:20 +0100

    Ajout d'une liste déroulante de films avec boutons pour déplier à la homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "../style/Home.css";
+import WidgetMovies from "../components/WidgetMovies";
 
 function Home() {
-  return <div />;
+  return (
+    <div>
+      <div className="display-movies">
+        <h1>New entries this week</h1>
+        <WidgetMovies />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
